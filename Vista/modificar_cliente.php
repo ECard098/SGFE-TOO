62335f22a41f02a758b888f8d7da8dd44f6898ce
Jaime Cardona <148599920+ECard098@users.noreply.github.com>
2024-11-12 12:18:18 -0600

    Modificar cliente implementado

--- Vista/modificar_cliente.php
+++ Vista/modificar_cliente.php
@@ -1,8 +1,15 @@
 <?php
+
 session_start();
 if (empty($_SESSION["id"])) {
     header("location: ../index.php");
 }
+
+include "../Modelo/conexion.php";
+$id = $_GET["id"];
+$sql = $conexion->query("select * from cliente where id_Cliente = $id");
+
+
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -32,13 +39,13 @@ if (empty($_SESSION["id"])) {
                         <li class="nav-item"><a href="#" class="nav-link text-white">Reservas</a></li>
                         <li class="nav-item"><a href="#" class="nav-link text-white">Pagos</a></li>
                         <li class="nav-item"><a href="#" class="nav-link text-white">Reportes</a></li>
-                         <li class="nav-item"><a href="#" class="nav-link text-white">Expediente</a>
+                        <li class="nav-item"><a href="#" class="nav-link text-white">Expediente</a>
                         </li>
                         <li class="nav-item">
                             <div class="dropdown">
                                 <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     <?php
-                                    echo $_SESSION["nombre"] . " - ". $_SESSION["correo"];
+                                    echo $_SESSION["nombre"] . " - " . $_SESSION["correo"];
                                     ?>
                                 </button>
                                 <ul class="dropdown-menu">
@@ -53,59 +60,62 @@ if (empty($_SESSION["id"])) {
     </header>
 
     <div class="container-fluid">
-            <div class="col-md-12 p-5">
-                <div class="card p-3 m-2">
-                    <div class="card-header">
-                        <h2 class="text-center">Editar cliente</h2>
-                        <?php
-                        include "../Modelo/conexion.php";
-                        include "../Controlador/personaControler.php";                       
-                        ?>
-                    </div>
-                    <form class="m-3 p-3" method="POST" action="">
+        <div class="col-md-12 p-5">
+            <div class="card p-3 m-2">
+                <div class="card-header">
+                    <h2 class="text-center">Editar cliente</h2>
+                </div>
+                <form class="m-3 p-3" method="POST" action="">
+                    <input type="hidden" name="id" value="<?=$_GET["id"]?>">
+                    <?php
+                    include "../Controlador/modificarPersona.php";
+                    while ($datos = $sql->fetch_Object()) { ?>
                         <div class="mb-2">
                             <label class="form-label">Nombre: </label>
-                            <input type="text" class="form-control" name="nombreC" placeholder="Ingresa nombre cliente" required maxlength="50">
+                            <input type="text" class="form-control" name="nombreC" placeholder="Ingresa nombre cliente" required maxlength="50" value="<?= $datos->nombre ?>">
                         </div>
                         <div class="mb-2">
                             <label class="form-label">Apellido: </label>
-                            <input type="text" class="form-control" name="apellidoC" placeholder="Ingresa apellido cliente" required maxlength="50">
+                            <input type="text" class="form-control" name="apellidoC" placeholder="Ingresa apellido cliente" required maxlength="50" value="<?= $datos->apellido ?>">
                         </div>
                         <div class="mb-2">
                             <label class="form-label">Correo: </label>
-                            <input type="email" class="form-control" name="correoC" placeholder="Ingresa correo cliente" required>
+                            <input type="email" class="form-control" name="correoC" placeholder="Ingresa correo cliente" required value="<?= $datos->correo ?>">
                         </div>
+
                         <div class="mb-2">
-                            <label class="form-label">Genero: </label>
+                            <label class="form-label">Género: </label>
                             <select class="form-select" name="generoC" aria-label="Eliga una opción">
-                                <option selected>Seleccione opción</option>
-                                <option value="M">Masculino</option>
-                                <option value="F">Femenino</option>
-                                <option value="Otro">Otro</option>
+                                <option value="M" <?= $datos->genero == 'M' ? 'selected' : '' ?>>Masculino</option>
+                                <option value="F" <?= $datos->genero == 'F' ? 'selected' : '' ?>>Femenino</option>
+                                <option value="Otro" <?= $datos->genero == 'Otro' ? 'selected' : '' ?>>Otro</option>
                             </select>
                         </div>
 
                         <div class="mb-2">
                             <label class="form-label">Fecha Nacimiento: </label>
-                            <input type="date" class="form-control" name="fechaC" placeholder="Ingresa apellido cliente" required>
+                            <input type="date" class="form-control" name="fechaC" placeholder="Ingresa apellido cliente" required value="<?= $datos->fecha_Nacimiento ?>">
                         </div>
                         <div class="mb-2">
                             <label class="form-label">Telefono: </label>
-                            <input type="text" class="form-control" name="telefonoC" placeholder="Ingresa telefono cliente" required maxlength="8">
+                            <input type="text" class="form-control" name="telefonoC" placeholder="Ingresa telefono cliente" required maxlength="8" value="<?= $datos->telefono ?>">
                         </div>
                         <div class="mb-2">
                             <label class="form-label">Dirección: </label>
-                            <input type="text" class="form-control" name="direccionC" placeholder="Ingresa dirección cliente" required>
+                            <input type="text" class="form-control" name="direccionC" placeholder="Ingresa dirección cliente" required value="<?= $datos->direccion ?>">
                         </div>
-                        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                            <button type="submit" class="btn btn-primary" value="ok" name="btnRegistrar">Actualizar cliente</button>
-                            <a class="btn btn-secondary" href="./lista_cliente.php" role="button">Volver al listado</a>
-                        </div>
+                    <?php }
+                    ?>
 
-                    </form>
-                </div>
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                        <button type="submit" class="btn btn-primary" value="ok" name="btnRegistrar">Modificar cliente</button>
+                        <a class="btn btn-secondary" href="./lista_cliente.php" role="button">Volver al listado</a>
+                    </div>
+
+                </form>
             </div>
-        
+        </div>
+
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         <script src="https://kit.fontawesome.com/80d40214cc.js" crossorigin="anonymous"></script>
