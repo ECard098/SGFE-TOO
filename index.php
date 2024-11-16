8bff6da178c39ed6a954e7292856e0eeaeb6d5bd
Jaime Cardona <148599920+ECard098@users.noreply.github.com>
2024-11-15 21:57:01 -0600

    C:

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <title>Iniciar sesión</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="./Vista/CSS/login.css">
+    <link rel="icon" href="./IMG/ataud.ico" type="image/x-icon">
 </head>
 
 <body class="d-flex align-items-center justify-content-center vh-100">
